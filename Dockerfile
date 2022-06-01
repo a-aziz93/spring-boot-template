5438ba3b4ce583c662aa4a9cda1ff095ff7e93d9
a-aziz93 <57552803+a-aziz93@users.noreply.github.com>
2022-06-01 23:58:48 +0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM amazoncorretto:11-alpine-jdk
+MAINTAINER ai.com
+ENV APP_HOME=/usr/app/
+WORKDIR $APP_HOME
+COPY build/libs/*.jar spring-boot-template-1.0.0.jar
+ENV PORT 5555
+EXPOSE 5555
+ENTRYPOINT ["java","-jar","spring-boot-template-1.0.0.jar"]
---
+++ src/test/kotlin/ai/tech/springboottemplate/SpringBootTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package ai.tech.springboottemplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootTemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
