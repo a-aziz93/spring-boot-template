edb755f11b9de2a50ee75a00705dcbb6415616a4
a-aziz93 <57552803+a-aziz93@users.noreply.github.com>
2022-06-03 01:32:14 +0500

    Initial commit

--- .space.kts
+++ .space.kts
@@ -10,7 +10,7 @@ job("Build and run tests") {
         schedule { cron("0 8 * * *") }
     }
     container(displayName = "Continuous Inspection of code quality and security", image = "sonarsource/sonar-scanner-cli"){
-        env["SONAR_HOST_URL"]="http://sonarqube"
+        env["SONAR_HOST_URL"]="http://sonarqube:9000"
 service("bitnami/sonarqube"){
 alias("sonarqube")
     env["ALLOW_EMPTY_PASSWORD"]="yes"
