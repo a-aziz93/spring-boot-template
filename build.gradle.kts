895856812f2986e87df80600d0ec3b67b78fbcad
a-aziz93 <57552803+a-aziz93@users.noreply.github.com>
2022-06-05 01:51:59 +0500

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,9 @@ plugins {
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
+    `java-library`
     `maven-publish`
+    signing
 }
 
 group = "ai.tech"
@@ -50,13 +52,47 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("spring-boot-template")
+                description.set("Spring Boot microservice template")
+                url.set("https://github.com/a-aziz93/spring-boot-template/")
+                properties.set(mapOf(
+                
+                ))
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("a-aziz93")
+                        name.set("Atoev Aziz")
+                        email.set("a.atoev93@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/a-aziz93/spring-boot-template.git")
+                    developerConnection.set("scm:git:ssh://github.com/a-aziz93/spring-boot-template.git")
+                    url.set("https://github.com/a-aziz93/spring-boot-template/")
+                }
+            }
         }
     }
     
     repositories {
         maven {
             // change to point to your repo, e.g. http://my.org/repo
-            url = uri(layout.buildDirectory.dir("https://maven.pkg.jetbrains.space/aaziz93/p/microservices/maven"))
+            url = uri("https://maven.pkg.jetbrains.space/aaziz93/p/microservices/maven")
             credentials {
                 // Automation has a special account for authentication in Space
                 // account credentials are accessible via env vars
@@ -64,6 +100,16 @@ publishing {
                 password = System.getenv("JB_SPACE_CLIENT_SECRET")
             }
         }
+    }
+}
+
+signing {
+    sign(publishing.publications["maven"])
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava11Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
     }
 }
 
