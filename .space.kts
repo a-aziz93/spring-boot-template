4dcf2329682528c49946aaf9dac31cde3fb80644
a-aziz93 <57552803+a-aziz93@users.noreply.github.com>
2022-06-04 23:55:46 +0500

    Initial commit

--- .space.kts
+++ .space.kts
@@ -9,13 +9,13 @@ job("Code analysis, test, build and push") {
         gitPush { enabled =true }
         schedule { cron("0 8 * * *") }
     }
-   /*
+   
     container(displayName = "Sonar continuous inspection of code quality and security", image = "sonarsource/sonar-scanner-cli")
     {
         env["SONAR_LOGIN"] = Secrets("sonar_token")
         env["SONAR_HOST_URL"] = Params("sonar_host_url")
         args("-Dsonar.projectKey=a-aziz93_spring-boot-template","-Dsonar.organization=a-aziz93")
-    }*/
+    }
     
     container(displayName = "Gradle test and build", image = "openjdk:11") {
         shellScript {
@@ -31,7 +31,6 @@ job("Code analysis, test, build and push") {
             cpu = 1.cpu
             memory = 64.mb
         }
-        shellScript { content="ls $mountDir/share/build/libs" }
-       //args("jib","jar","--target=aaziz93.registry.jetbrains.space/p/microservices/containers/spring-boot-template:1.0.\$JB_SPACE_EXECUTION_NUMBER","$mountDir/share/build/libs/spring-boot-template-*.jar")
+        args("jib","jar","--target=aaziz93.registry.jetbrains.space/p/microservices/containers/spring-boot-template:1.0.\$JB_SPACE_EXECUTION_NUMBER","$mountDir/share/build/libs/spring-boot-template-*.jar")
     }
 }
