bbdf2a7e1f21f55cd4472228f2caebd7b9be7179
a-aziz93 <57552803+a-aziz93@users.noreply.github.com>
2022-06-04 13:50:29 +0500

    Initial commit

--- .space.kts
+++ .space.kts
@@ -4,7 +4,7 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Code analysis, build and run tests") {
+job("Code analysis, run tests, build and push artifact") {
     startOn {
         gitPush { enabled=true }
         schedule { cron("0 8 * * *") }
@@ -16,7 +16,7 @@ job("Code analysis, build and run tests") {
         
     }
     
-    container(displayName = "Gradle build", image = "openjdk:11") {
+    container(displayName = "Gradle test and build", image = "openjdk:11") {
         kotlinScript { api ->
             // here goes complex logic
             api.gradlew("clean")
