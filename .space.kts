6a828e75f982c1b6a05c08f897084ba5a677b918
a-aziz93 <57552803+a-aziz93@users.noreply.github.com>
2022-06-06 02:49:00 +0500

    Initial commit

--- .space.kts
+++ .space.kts
@@ -35,7 +35,7 @@ job("Code analysis, test, build and push") {
         shellScript {
             content = """
                 ARTIFACT_NAME=`cat $mountDir/${getArtifactFilePath()}`
-                jib jar --target=aaziz93.registry.jetbrains.space/p/microservices/containers/"${'$'}ARTIFACT_NAME" $mountDir/share/build/libs/"${'$'}ARTIFACT_NAME".jar
+                jib jar --to-username=${Params("space_docker_registry_user")} --to-password=${Secrets("space_docker_registry_token")} --target=aaziz93.registry.jetbrains.space/p/microservices/containers/"${'$'}ARTIFACT_NAME" $mountDir/share/build/libs/"${'$'}ARTIFACT_NAME".jar
             """
         }
     }
