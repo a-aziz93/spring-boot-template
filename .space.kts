1e291ad005c09ade42adbce44d425a798b5ff9c9
a-aziz93 <57552803+a-aziz93@users.noreply.github.com>
2022-06-05 22:55:52 +0500

    Initial commit

--- .space.kts
+++ .space.kts
@@ -36,6 +36,9 @@ job("Code analysis, test, build and push") {
             cpu = 1.cpu
             memory = 2000.mb
         }
-        args("jib","jar","--target=aaziz93.registry.jetbrains.space/p/microservices/containers/spring-boot-template:1.0.0","\$(<$mountDir/share/artifact.txt)")
+        shellScript {
+            content =
+                "jib jar --target=aaziz93.registry.jetbrains.space/p/microservices/containers/spring-boot-template:1.0.0 \$(<$mountDir/share/artifact.txt)"
+        }
     }
 }
