dd191fa66106b5816fdc095276c14eb5039e8534
a-aziz93 <57552803+a-aziz93@users.noreply.github.com>
2022-06-06 02:16:00 +0500

    Initial commit

--- .space.kts
+++ .space.kts
@@ -35,7 +35,7 @@ job("Code analysis, test, build and push") {
         shellScript {
             content = """
                 ARTIFACT_NAME=`cat $mountDir/${getArtifactFilePath()}`
-                jib jar --target=aaziz93.registry.jetbrains.space/p/microservices/containers/"${'$'}ARTIFACT_NAME" $mountDir/share/build/libs/"${'$'}ARTIFACT_NAME"
+                jib jar --target=aaziz93.registry.jetbrains.space/p/microservices/containers/"${'$'}ARTIFACT_NAME" $mountDir/share/build/libs/"${'$'}ARTIFACT_NAME".jar
             """
         }
     }
