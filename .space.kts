268b5d9ea8f9bccd8e8f02e3e0b73998695d3d65
a-aziz93 <57552803+a-aziz93@users.noreply.github.com>
2022-06-08 01:29:41 +0500

    Initial commit

--- .space.kts
+++ .space.kts
@@ -4,10 +4,47 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
+job("Build Fleet indexes") {
+    // ide is an IDE you want Space to build indexes for:
+    // for JetBrains Fleet - Ide.Fleet
+    // for IntelliJ-based IDEs via Gateway -
+    // Ide.Idea, Ide.WebStorm, Ide.RubyMine,
+    // Ide.CLion, Ide.GoLand, Ide.PhpStorm,
+    // Ide.PyCharm, Ide.Rider
+    
+    // optional
+    startOn {
+        // run on schedule every day at 5AM
+        schedule { cron("0 5 * * *") }
+        // run on every commit...
+        gitPush {
+            // ...but only to the main branch
+            branchFilter {
+                +"refs/heads/main"
+            }
+        }
+    }
+    
+    warmup(ide = Ide.Fleet) {
+        // this job will run in a container based on the image
+        // specified in the devfile.yaml (if specified) and for ide version (if specified)
+        devfile = ".space/devfile.yaml"
+        // optional custom script
+        scriptLocation = "./dev-env-warmup.sh"
+    }
+    
+    // optional
+    git {
+        // fetch the entire commit history
+        depth = UNLIMITED_DEPTH
+        // fetch all branches
+        refSpec = "refs/*:refs/*"
+    }
+}
+
 job("Code analysis, test, build and push") {
     startOn {
         gitPush { enabled =true }
-        schedule { cron("0 8 * * *") }
     }
    
     container(displayName = "Sonarqube continuous inspection of code quality and security", image = "openjdk:11")
@@ -25,8 +62,8 @@ job("Code analysis, test, build and push") {
     }
     
     container("Jib build docker container and publish to space docker registry", image = "openjdk:11") {
-        kotlinScript { api->
-            api.gradlew("jib","-Djib.to.auth.username=\"${'$'}JB_SPACE_CLIENT_ID\"","-Djib.to.auth.password=\"${'$'}JB_SPACE_CLIENT_SECRET\"")
+        shellScript {
+            content="./gradlew jib -Djib.to.auth.username=\"${'$'}JB_SPACE_CLIENT_ID\" -Djib.to.auth.password=\"${'$'}JB_SPACE_CLIENT_SECRET\""
         }
     }
 }
